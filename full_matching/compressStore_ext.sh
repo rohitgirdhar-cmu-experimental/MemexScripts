63d8074494a2867e74e0238b1251234c5595239d
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-07-26 18:28:36 +0000

    fix

--- full_matching/compressStore_ext.sh
+++ full_matching/compressStore_ext.sh
@@ -2,5 +2,5 @@ CODE_PATH=/home/rgirdhar/data/Work/Code/0002_Retrieval/Try/ScalableLSH/DiskE2LSH
 nice -n 10 $CODE_PATH/compressStore.bin \
        -i ~/memexdata/Dataset/processed/0006_BackpageExtended/Features/CNN/pool5_normed/ \
        -o ~/memexdata/Dataset/processed/0006_BackpageExtended/Features/CNN/pool5_normed_compressed/ \
-       -k /home/rgirdhar/memexdata/Dataset/processed/0006_BackpageComplete/Images/lists/feature_ids.txt \
+       -k /home/rgirdhar/memexdata/Dataset/processed/0006_BackpageExtended/Images/lists/feature_ids.txt \
        -s 8236837
